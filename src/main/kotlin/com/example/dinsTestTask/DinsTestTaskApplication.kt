0c34c20aa428b1681ab44914fc84b14c7705c5ad
toktobubu <torolbaeva@beeline.kz>
2020-07-19 15:38:40 +0600

    First commit

---
+++ src/main/kotlin/com/example/dinsTestTask/DinsTestTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dinsTestTask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DinsTestTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<DinsTestTaskApplication>(*args)
+}
